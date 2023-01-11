bce26b1a6e8acd87f2d2fbb574bd2c88c6bc5694
AndreTheDeveloper <andre.colaris98@live.ca>
2023-01-10 22:19:54 -0700

    Created blank activity to start building the layout of the app

---
+++ app/src/main/java/com/andresapps/bestbeforedates/client/CurrentList.java
@@ -0,0 +1,15 @@
+package com.andresapps.bestbeforedates.client;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.andresapps.bestbeforedates.R;
+
+public class CurrentList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_list);
+    }
+}
